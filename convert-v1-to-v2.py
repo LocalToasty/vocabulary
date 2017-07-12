ea02a3672795136dfd6aff733551ccb1e31dc1a9
Marko van Treeck <markovantreeck@gmail.com>
2017-07-12 21:52:14 +0200

    Implement time based card selection mechanism

---
+++ convert-v1-to-v2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import random
+import pickle
+import sys
+import json
+import time
+
+class Card:
+    pass
+
+class Database:
+    pass
+
+
+class DatabaseEncoder(json.JSONEncoder):
+    def default(self, db):
+        words = []
+        for i, cat in enumerate(db.categories):
+            for word in cat:
+                proficiency = 24*60*60*i*i + random.random() * 0.2 * 24*60*60*i*i
+                words.append({
+                    "entries": word.words,
+                    "comment": word.comment,
+                    "proficiency": proficiency,
+                    "due": time.time() + proficiency
+                })
+        
+        return {
+            "langs": db.langs,
+            "cards": words
+        }
+    
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: {} <file>".format(sys.argv[0]))
+        return
+
+    db = None
+    with open(sys.argv[1], "rb") as dbfile:
+        db = pickle.load(dbfile)
+
+    with open(sys.argv[1] + ".new", "w") as dbfile:
+        dbfile.write(json.dumps(db, cls=DatabaseEncoder, indent=2))
+
+
+if __name__ == "__main__":
+    main()
--- vocabulary.py
+++ vocabulary.py
@@ -1,329 +1,219 @@
 #!/usr/bin/env python3
 
 import random
-import numpy.random
 import pickle
 import sys
+import json
+import time
+import heapq
+
+path = ""
 
 class Card:
-    def __init__(self, words, comment):
-        self.words = words
+    def __init__(self, entries, comment):
+        self.entries = entries
         self.comment = comment
+        self.proficiency = 0.
+        self.due = time.time()
+
+    def is_due(self):
+        return time.time() >= self.due
 
     def __str__(self):
-        res = self.words[0]
-
-        for lang in self.words[1:]:
-            res += "\t{}".format(lang)
-
+        res = str(self.entries[0])
+        for e in self.entries[1:]:
+            res += "\t" + e
         if self.comment:
             res += "\t# " + self.comment
-
         return res
 
-    def __hash__(self):
-        res = hash(self.comment)
-        for lang in self.words:
-            res += hash(lang)
+    def __lt__(self, other):
+        return self.due < other.due
 
-        return res
 
 class Database:
-    """Contains multiple words."""
     def __init__(self, langs):
         self.langs = langs
-        self.categories = [set()]
-        self.modified = False
-        self.path = ""
+        self.cards = []
+        self.changes = False
 
-    def __len__(self):
-        return self.no_of_cards_easier_than(0)
+    def from_dict(dct):
+        db = Database(dct["langs"])
+        n = 0
+        for c in dct["cards"]:
+            n += 1
+            card = Card(c["entries"], c["comment"])
+            card.proficiency = c["proficiency"]
+            card.due = c["due"]
+            db.cards.append(card)
+        heapq.heapify(db.cards)
+        print("Loaded", n, "cards")
+        return db
 
-    def no_of_cards_easier_than(self, difficulty):
-        length = 0
-        for category in self.categories[difficulty:]:
-            length += len(category)
+    def add(self, card):
+        self.changes = True
+        heapq.heappush(self.cards, card)
 
-        return length
+    def pop(self):
+        self.changes = True
+        return heapq.heappop(self.cards)
 
-    def add_card(self, word):
-        """Adds a new word to the data base.
+    def top(self):
+        return self.cards[0]
 
-        The word is given a difficulty of 0 (i.e. unknown)."
-        """
-        self.categories[0].add(word)
-        self.modified = True
-
-    def update(self, correct, incorrect):
-        if len(correct) == len(self.categories) and correct[-1]:
-            self.categories += [set()]
-
-        for difficulty, words in enumerate(correct):
-            if words:
-                self.categories[difficulty] -= words
-                self.categories[difficulty + 1].update(words)
-
-        for difficulty, words in enumerate(incorrect[1:]):
-            self.categories[difficulty + 1] -= words
-            self.categories[0].update(words)
-
-        self.modified = True
+class DatabaseEncoder(json.JSONEncoder):
+    def default(self, db):
+        return {
+            "langs": db.langs,
+            "cards": [{
+                "entries": card.entries,
+                "comment": card.comment,
+                "proficiency": card.proficiency,
+                "due": card.due
+            } for card in db.cards]
+        }
 
 
 def main():
-    if not len(sys.argv):
+    if len(sys.argv) != 2:
         print("Usage: {} <file>".format(sys.argv[0]))
         return
 
-    db = load_database(sys.argv[1])
-    main_menu(db)
+    db = None
+    with open(sys.argv[1], "r") as dbfile:
+        global path
+        path = sys.argv[1]
+        db = Database.from_dict(json.load(dbfile))
 
-
-def load_database(path):
-    database = None
-    with open(path, "rb") as dbfile:
-        database = pickle.load(dbfile)
-
-    database.modified = False
-    database.path = path
-
-    return database
-
-def main_menu(database):
-    print("Vocabulary training program")
+    if db.top():
+        if  db.top().is_due():
+            print("Cards ready for repetition")
+        else:
+            print("Next card ready on", time.asctime(time.localtime(db.top().due)))
 
     while True:
         try:
-            answer = input("Select an option: ")
+            choice = input("> ")
+        except (KeyboardInterrupt, EOFError):
+            break
 
-            if answer in ['A', 'a']:
-                word = promt_for_card(database.langs)
-                if word:
-                    database.add_card(word)
+        if choice in ["a", "A"]:
+            add_card(db)
+        if choice in ["r", "R"]:
+            remove_card(db)
+        elif choice in ["q", "q"]:
+            learn(db)
+        elif choice in ["l", "l"]:
+            list_cards(db)
+        elif choice in ["f", "F"]:
+            find(db)
+        elif choice in ["s", "S"]:
+            save(db)
 
-            if answer in ['R', 'r']:
-                promt_remove_card(database)
+    print()
+    if db.changes and ask_yes_no("Save changes?", default=True):
+        save(db)
 
-            elif answer in ['Z', 'z']:
-                take_quiz(database)
+    print("Come back on",
+          time.asctime(time.localtime(max(time.time(),
+                                          heapq.nsmallest(min(24, len(db.cards)), db.cards)[-1].due))))
 
-            elif answer in ['L', 'l']:
-                learn_cards(database)
-
-            elif answer in ['I', 'i']:
-                list_cards(database)
-
-            elif answer in ['F', 'f']:
-                keyword = input("Enter term to search for: ")
-                for difficulty, category in enumerate(database.categories):
-                    for card in category:
-                        if any(keyword in word for word in card.words) or keyword in card.comment:
-                            print("{:3}".format(difficulty), card)
-
-            elif answer in ['T', 't']:
-                print_statistics(database)
-
-            elif answer in ['S', 's']:
-                path = input("Save database as [{}]: "
-                             .format(database.path))
-                if path == "":
-                    path = database.path
-                with open(path, "wb") as dbfile:
-                    pickle.dump(database, dbfile)
-                database.path = path
-
-            elif answer in ['C', 'c']:
-                database = None
-
-            elif answer in ['Q', 'q']:
-                break
-
-            else:
-                print("[A]dd new card")
-                print("[R]emove a card")
-                print("Take a qui[z]")
-                print("[L]earn new words")
-                print("L[i]st words")
-                print("[F]ind words")
-                print("S[t]atistics")
-                print("[S]ave database")
-                print("[C]lose database")
-                print("[Q]uit")
-
-        except KeyboardInterrupt:
-            if database.modified:
-                answer = input("Save changes? [Y/n] ")
-                if answer not in ['N', 'n']:
-                    path = input("Save database as [{}]: "
-                                 .format(database.path))
-                    if path == "":
-                        path = database.path
-                    with open(path, "wb") as dbfile:
-                        pickle.dump(database, dbfile)
-
-            return
-
-
-def promt_for_database():
-    lang_no = int(input("How many languages? "))
-    langs = []
-    for i in range(lang_no):
-        lang = input("Enter name of language {}: ".format(i + 1))
-        langs += [lang]
-
-    return Database(langs)
-
-
-def promt_for_card(langs):
+def add_card(db):
     try:
-        words = []
-        for lang in langs:
-            word = input("Enter your word in {}: ".format(lang))
-            words.append(word)
+        entries = []
+        for lang in db.langs:
+            entry = input(lang + ": ")
+            entries.append(entry)
 
-        comment = input("Enter comment: ")
-        return Card(words, comment)
+        comment = input("Comment: ")
+        db.add(Card(entries, comment))
 
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, EOFError):
         print()
         return None
 
-
-def promt_remove_card(database):
+def remove_card(db):
     content = input("Enter card to remove: ")
-    to_remove = None
-    for category in database.categories:
-        for card in category:
-            if content in card.words:
-                to_remove = (category, card)
+    for card in db.cards:
+        if content in card.entries:
+            print("Removed {}".format(card))
+            db.cards.remove(card)
+            heapq.heapify(db.cards)
 
-    if to_remove is not None:
-        category, card = to_remove
-        print("Removed {}".format(card))
-        category.remove(card)
-
-
-def take_quiz(database):
-    try:
-        length = int(input("How many words should the quiz contain? "))
-        quiz = make_quiz(database.categories, length)
-        correct, incorrect = do_quiz(database, quiz)
-        database.update(correct, incorrect)
-
-    except KeyboardInterrupt:
+def learn(db):
+    if not db.cards or not db.top().is_due():
+        print("No cards to learn")
         return
 
+    duration = 0
+    while True:
+        try:
+            duration = float(input("Duration (min): "))
+            break
+        except ValueError:
+            pass
 
-def make_quiz(words, length, p=1/3):
-    """Selects words for a quiz.
+    end_time = time.time() + duration * 60
 
-    words -- a list containing sets of words to form the quiz from
-    n     -- length of questions in the quiz
+    while time.time() < end_time:
+        if not db.top().is_due():
+            print("Next cards ready on", time.asctime(time.localtime(db.top().due)))
+            break
 
-    `words` is a list of sets of words, where `words[0]` contains words which
-    the trainee is most inexperienced with, `words[1]` those they know better,
-    and so forth.  The final quiz will contain n/(i+1) words from the list
-    `words[i]`.
-
-    If there are not enough words of a certain difficulty, words from the next
-    harder one will be selected.
-
-    """
-
-    if not words or length == 0:
-        return []
-
-    # number of words from the list `words[0]`
-    words_from_this_category = min(numpy.random.binomial(length, p), len(words[0]))
-    # number of words from the lists `words[1:]`
-    words_from_other_categories = length - words_from_this_category
-
-    quiz = [random.sample(words[0], words_from_this_category)]
-    quiz += make_quiz(words[1:], words_from_other_categories, p)
-
-    return quiz
-
-
-def learn_cards(database):
-    try:
-        length = int(input("How many words should the quiz contain? "))  #TODO
-        quiz = select_cards_to_learn(database.categories, length)
-        correct, incorrect = do_quiz(database, quiz)
-        database.update(correct, incorrect)
-
-    except KeyboardInterrupt:
-        return
-
-
-def select_cards_to_learn(words, length, p=2/3):
-    if not words or length == 0:
-        return []
-
-    words_from_this_category = min(numpy.random.binomial(length, p), len(words[0]))
-    words_from_other_categories = length - words_from_this_category
-
-    quiz = [x for x in words[0]]
-    quiz.sort(key=hash)
-    quiz = [quiz[:words_from_this_category]]
-    quiz += make_quiz(words[1:], words_from_other_categories, p)
-
-    return quiz
-    
-
-
-def do_quiz(database, quiz):
-    print("Which language should be shown?")
-    for i, lang in enumerate(database.langs):
-        print("{}: {}".format(i, lang))
-
-    from_lang = int(input("> "))
-
-    correct = [set() for _ in range(len(quiz))]
-    incorrect = [set() for _ in range(len(quiz))]
-
-    try:
-        for difficulty, cards in enumerate(quiz):
-            for question in cards:
-                print(question.words[from_lang])
-
-                input("Press [enter] to show solution")
-                print(question)
-
-                res = input("Did you answer correctly? [y/N] ")
-                if res in ['Y', 'y']:
-                    correct[difficulty].add(question)
-                else:
-                    incorrect[difficulty].add(question)
-
-    finally:
-        print("{}/{} answered correctly".format(flat_len(correct), flat_len(correct) + flat_len(incorrect)))
-
-        return (correct, incorrect)
-
-
-def flat_len(xss):
-    return sum([len(xs) for xs in xss])
-
-
-def print_statistics(database):
-    for difficulty, category in enumerate(database.categories):
-        print("{:5}: {:6} words".format(difficulty, len(category)))
-
-    print("total: {:6} words".format(len(database)))
-
-
-def list_cards(database):
-    line = 0
-    for difficulty, category in enumerate(database.categories):
-        print("### Difficulty {} ###".format(difficulty))
-        line += 1
-        for card in category:
-            if line >= 20:
-                input()
-                line = 0
-
+        try:
+            card = db.pop()
+            print(random.choice(card.entries), end=" ")
+            input()
             print(card)
-            line += 1
+
+            if ask_yes_no("Correct?", default=False):
+                card.proficiency = card.proficiency * 1.5 + 0.2 * (time.time() - card.due)
+            else:
+                card.proficiency = 1
+            card.due = time.time() + card.proficiency
+            db.add(card)
+        except (KeyboardInterrupt, EOFError):
+            db.add(card)
+            break
+
+
+def ask_yes_no(question, default):
+    print(question, end=" ")
+    if default:
+        print("[Y/n]: ", end="")
+    else:
+        print("[y/N]: ", end="")
+    answer = input()
+    if answer in ["y", "Y"]: return True
+    elif answer in ["n", "N"]: return False
+    else: return default
+
+
+def find(db):
+    keyword = input("Seach for: ")
+    for card in db.cards:
+        if any(keyword in entry for entry in card.entries) or keyword in card.comment:
+            print(time.asctime(time.localtime(card.due)), card)
+
+def list_cards(db):
+    cards = db.cards.copy()
+    while cards:
+        card = heapq.heappop(cards)
+        print(time.asctime(time.localtime(card.due)), card)
+
+
+def save(db):
+    global path
+    if path:
+        new_path = input("Save as [" + path + "]: ")
+        if new_path:
+            path = new_path
+    else:
+        path = input("Save as: ")
+
+    with open(path, "w") as dbfile:
+        dbfile.write(json.dumps(db, cls=DatabaseEncoder, indent=2))
+    db.changes = False
 
 
 if __name__ == "__main__":
