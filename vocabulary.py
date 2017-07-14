47ba4bb011f3088519ec1c7d5bce8ad419dd37e2
Marko van Treeck <markovantreeck@gmail.com>
2017-07-15 01:12:59 +0200

    Match anywhere in string when trying to find entry

--- vocabulary.py
+++ vocabulary.py
@@ -229,7 +229,7 @@ def ask_yes_no(question, default):
 def find(db):
     prog = None
     try:
-        prog = re.compile(input("Seach for: "))
+        prog = re.compile(".*" + input("Seach for: "))
     except re.error as e:
         print("Error while compiling regular expression:", e.msg)
         return
