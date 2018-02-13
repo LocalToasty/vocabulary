6b3fc3dc5b23799b2cd073eb9e2fcf9cf1f4de75
Marko van Treeck <markovantreeck@gmail.com>
2018-02-13 10:27:20 +0100

    Refactor data structure and vocab program into seperate files

---
+++ clivocab.py
@@ -0,0 +1,209 @@
+#!/usr/bin/python3
+
+from vocabulary import *
+import sys
+import random
+import re
+from math import log
+from typing import Optional
+
+class VocabularyApp:
+    def __init__(self) -> None:
+        if len(sys.argv) != 2:
+            print("Usage: {} <file>".format(sys.argv[0]))
+            return
+
+        global path
+        self.path = sys.argv[1]
+
+
+    def run(self) -> None:
+        self.db = None # type: Optional[Database]
+        try:
+            self.db = Database.load(self.path)
+        except FileNotFoundError:
+            n = int(input("How many entries per card? "))
+            langs = []
+            for i in range(n):
+                langs.append(input("Name of entry {}: ".format(i)))
+            self.db = Database(langs)
+
+        if self.db.cards:
+            if self.db.top().is_due():
+                print("Cards ready for repetition")
+            else:
+                print("Next card ready on", time.asctime(time.localtime(self.db.top().due_at())))
+
+        while True:
+            try:
+                choice = input("> ")
+            except (KeyboardInterrupt, EOFError):
+                break
+
+            try:
+                if choice in ["a", "A"]:
+                    self.add_card()
+                if choice in ["r", "R"]:
+                    self.remove_card()
+                elif choice in ["l", "l"]:
+                    self.learn()
+                elif choice in ["f", "F"]:
+                    self.find()
+                elif choice in ["t", "T"]:
+                    self.stats()
+                elif choice in ["s", "S"]:
+                    self.save()
+            except (KeyboardInterrupt, EOFError):
+                pass
+
+        print()
+        if self.db.changes and ask_yes_no("Save changes?", exact=False, default=True):
+            self.save()
+
+    def add_card(self) -> None:
+        try:
+            entries = [] # type: List[Entry]
+            for lang in self.db.langs:
+                text = input(lang + ": ")
+                if text == '"""':
+                    text = multiline_input()
+                entries.append(Entry(text))
+    
+            comment = input("Comment: ")
+            self.db.add(Card(entries, comment))
+    
+        except (KeyboardInterrupt, EOFError):
+            print()
+            return
+    
+    
+    def remove_card(self) -> None:
+        content = input("Enter card to remove: ")
+        for card in self.db.cards:
+            if content in [e.text for e in card.entries]:
+                print("Removed {}".format(card))
+                self.db.cards.remove(card)
+                heapq.heapify(self.db.cards)
+                self.db.changes = True
+                break
+    
+    def learn(self) -> None:
+        if not self.db.cards or not self.db.top().is_due():
+            print("No cards to learn")
+            return
+    
+        duration = 0.
+        while True:
+            try:
+                duration = float(input("Duration (min): "))
+                break
+            except ValueError:
+                pass
+    
+        end_time = time.time() + duration * 60
+    
+        while time.time() < end_time:
+            if not self.db.top().is_due():
+                return
+    
+            try:
+                card = self.db.pop()
+                entry = card.due_entry()
+    
+                print(chr(27) + "[2J")
+                print(entry, end=" ")
+                input()
+                print(chr(27) + "[2J")
+                card.print()
+    
+                correct = ask_yes_no("Correct?")
+                self.db.retention[1] += entry.proficiency
+                if correct:
+                    self.db.retention[0] += entry.proficiency
+                    entry.proficiency = entry.proficiency * 2 + random.random() * 3600 * log((time.time() - entry.due)/3600 * 0.125 + 1) * 24 / log(24*0.125 + 1)
+                else:
+                    entry.proficiency = max(entry.proficiency / 16, 60.)
+                    self.db.retention[0] += entry.proficiency
+                entry.due = time.time() + entry.proficiency
+                self.db.add(card)
+            except (KeyboardInterrupt, EOFError):
+                self.db.add(card)
+                break
+    
+    def find(self) -> None:
+        prog = None
+        try:
+            prog = re.compile(".*" + input("Seach for: "))
+        except re.error as e:
+            print("Error while compiling regular expression:", e.msg)
+            return
+    
+        for card in self.db.cards:
+            if any(prog.match(entry.text) for entry in card.entries) or prog.match(card.comment):
+                print(time.asctime(time.localtime(card.due_at())), card)
+    
+    def stats(self) -> None:
+        print("Total:", len(self.db.cards))
+    
+        print("Retention score:", 100 * self.db.retention[0]/self.db.retention[1])
+    
+        #cards = self.db.cards.copy()
+        #n = 0
+        #while cards and heapq.heappop(cards).is_due():
+        #    n += 1
+        #print("Cards due:", n)
+    
+    def save(self) -> None:
+        if not self.db.changes:
+            print("No changes to be saved")
+            return
+    
+        while True:
+            try:
+                if self.path:
+                    new_path = input("Save as [" + self.path + "]: ")
+                    if new_path:
+                        self.path = new_path
+                else:
+                    self.path = input("Save as: ")
+    
+                if self.db.rethist and self.db.rethist[-1][0] + 60*60 < time.time():
+                    self.db.rethist += [[time.time(), self.db.retention[0]/self.db.retention[1]]]
+    
+                self.db.save(self.path)
+                break
+            except FileNotFoundError:
+                pass
+    
+        self.db.changes = False
+    
+    
+def multiline_input() -> str:
+    inp = input()
+    res = ""
+    while inp != '"""':
+        res += inp + "\n"
+        inp = input()
+
+    return res
+
+
+def ask_yes_no(question: str, exact: bool = True, default: bool = True) -> bool:
+    while True:
+        print(question, end=" ")
+        if exact:
+            print("[y/n]: ", end="")
+        elif default:
+            print("[Y/n]: ", end="")
+        else:
+            print("[y/N]: ", end="")
+
+        answer = input()
+        if answer in ["y", "Y"]: return True
+        elif answer in ["n", "N"]: return False
+        elif not exact and answer is "": return default
+    
+
+if __name__ == "__main__":
+    app = VocabularyApp()
+    app.run()
--- vocabulary.py
+++ vocabulary.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 
 import random
-import pickle
 import sys
 import json
 import time
 import heapq
-import re
-from math import log
 from typing import List
 
-path = ""
 
 class Entry:
     def __init__(self, text: str, proficiency: float = 60, due: float = None) -> None:
@@ -26,6 +22,7 @@ class Entry:
 
     def __lt__(self, other):
         return self.due < other.due
+
 
 class Card:
     def __init__(self, entries: List[Entry], comment: str) -> None:
@@ -62,6 +59,7 @@ class Card:
     def __lt__(self, other):
         return self.due_at() < other.due_at()
 
+
 class Database:
     def __init__(self, langs: List[str]) -> None:
         self.langs = langs
@@ -93,8 +91,8 @@ class Database:
             db.cards.append(card)
         heapq.heapify(db.cards)
 
-        if len(db.cards) > 64:
-            cards = heapq.nsmallest(64, db.cards)
+        if len(db.cards) > 56:
+            cards = heapq.nsmallest(56, db.cards)
             if cards[-1].is_due():
                 off = time.time() - cards[-1].due_at()
                 for card in db.cards:
@@ -113,6 +111,7 @@ class Database:
 
     def top(self):
         return self.cards[0]
+
 
 class DatabaseEncoder(json.JSONEncoder):
     def default(self, db):
@@ -133,204 +132,3 @@ class DatabaseEncoder(json.JSONEncoder):
         }
 
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: {} <file>".format(sys.argv[0]))
-        return
-
-    global path
-    path = sys.argv[1]
-
-    db = None
-    try:
-        db = Database.load(path)
-    except FileNotFoundError:
-        n = int(input("How many entries per card? "))
-        langs = []
-        for i in range(n):
-            langs.append(input("Name of entry {}: ".format(i)))
-        db = Database(langs)
-
-    if db.cards:
-        if  db.top().is_due():
-            print("Cards ready for repetition")
-        else:
-            print("Next card ready on", time.asctime(time.localtime(db.top().due_at())))
-
-    while True:
-        try:
-            choice = input("> ")
-        except (KeyboardInterrupt, EOFError):
-            break
-
-        try:
-            if choice in ["a", "A"]:
-                add_card(db)
-            if choice in ["r", "R"]:
-                remove_card(db)
-            elif choice in ["l", "l"]:
-                learn(db)
-            elif choice in ["f", "F"]:
-                find(db)
-            elif choice in ["t", "T"]:
-                stats(db)
-            elif choice in ["s", "S"]:
-                save(db)
-        except (KeyboardInterrupt, EOFError):
-            pass
-
-    print()
-    if db.changes and ask_yes_no("Save changes?", exact=False, default=True):
-        save(db)
-
-
-def add_card(db: Database):
-    try:
-        entries = []
-        for lang in db.langs:
-            text = input(lang + ": ")
-            if text == '"""':
-                text = multiline_input()
-            entries.append(Entry(text))
-
-        comment = input("Comment: ")
-        db.add(Card(entries, comment))
-
-    except (KeyboardInterrupt, EOFError):
-        print()
-        return None
-
-
-def multiline_input():
-    inp = input()
-    res = ""
-    while inp != '"""':
-        res += inp + "\n"
-        inp = input()
-
-    return res
-
-
-def remove_card(db: Database):
-    content = input("Enter card to remove: ")
-    for card in db.cards:
-        if content in [e.text for e in card.entries]:
-            print("Removed {}".format(card))
-            db.cards.remove(card)
-            heapq.heapify(db.cards)
-            db.changes = True
-            break
-
-def learn(db: Database):
-    if not db.cards or not db.top().is_due():
-        print("No cards to learn")
-        return
-
-    duration = 0.
-    while True:
-        try:
-            duration = float(input("Duration (min): "))
-            break
-        except ValueError:
-            pass
-
-    end_time = time.time() + duration * 60
-
-    while time.time() < end_time:
-        if not db.top().is_due():
-            return
-
-        try:
-            card = db.pop()
-            entry = card.due_entry()
-
-            print(chr(27) + "[2J")
-            print(entry, end=" ")
-            input()
-            print(chr(27) + "[2J")
-            card.print()
-
-            correct = ask_yes_no("Correct?")
-            db.retention[1] += entry.proficiency
-            if correct:
-                db.retention[0] += entry.proficiency
-                entry.proficiency = entry.proficiency * 2 + random.random() * 3600 * log((time.time() - entry.due)/3600 * 0.125 + 1) * 24 / log(24*0.125 + 1)
-            else:
-                entry.proficiency = max(entry.proficiency / 16, 60.)
-                db.retention[0] += entry.proficiency
-            entry.due = time.time() + entry.proficiency
-            db.add(card)
-        except (KeyboardInterrupt, EOFError):
-            db.add(card)
-            break
-
-
-def ask_yes_no(question: str, exact: bool = True, default: bool = True):
-    while True:
-        print(question, end=" ")
-        if exact:
-            print("[y/n]: ", end="")
-        elif default:
-            print("[Y/n]: ", end="")
-        else:
-            print("[y/N]: ", end="")
-
-        answer = input()
-        if answer in ["y", "Y"]: return True
-        elif answer in ["n", "N"]: return False
-        elif not exact and answer is "": return default
-
-
-def find(db: Database):
-    prog = None
-    try:
-        prog = re.compile(".*" + input("Seach for: "))
-    except re.error as e:
-        print("Error while compiling regular expression:", e.msg)
-        return
-
-    for card in db.cards:
-        if any(prog.match(entry.text) for entry in card.entries) or prog.match(card.comment):
-            print(time.asctime(time.localtime(card.due_at())), card)
-
-
-def stats(db: Database):
-    print("Total:", len(db.cards))
-
-    print("Retention score:", 100 * db.retention[0]/db.retention[1])
-
-    #cards = db.cards.copy()
-    #n = 0
-    #while cards and heapq.heappop(cards).is_due():
-    #    n += 1
-    #print("Cards due:", n)
-
-
-def save(db: Database):
-    if not db.changes:
-        print("No changes to be saved")
-        return
-
-    global path
-    while True:
-        try:
-            if path:
-                new_path = input("Save as [" + path + "]: ")
-                if new_path:
-                    path = new_path
-            else:
-                path = input("Save as: ")
-
-            if db.rethist and db.rethist[-1][0] + 60*60 < time.time():
-                db.rethist += [[time.time(), db.retention[0]/db.retention[1]]]
-
-            db.save(path)
-            break
-        except FileNotFoundError:
-            pass
-
-    db.changes = False
-
-
-if __name__ == "__main__":
-    main()
